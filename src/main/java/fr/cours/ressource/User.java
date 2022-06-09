c8ee7640034fc30fa9f704f6823d27b1778c8f55
zjxjohnxtlxb <zjxjohnxtlxb@outlook.com>
2022-06-09 15:17:32 +0200

    make it better

--- src/main/java/fr/cours/ressource/User.java
+++ src/main/java/fr/cours/ressource/User.java
@@ -23,7 +23,7 @@ public class User implements Serializable {
     @NotNull
     private String passwordHash;
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue
     private Long id;
 
     public User() {
